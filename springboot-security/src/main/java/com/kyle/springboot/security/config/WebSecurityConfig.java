381893641341add8fae1ddfc77ab8043847866a0
Kyle Liu <5201314.lkh@gmail.com>
2020-07-11 00:14:58 +0800

    添加简单模板，支持USER和ADMIN两种角色的用户分别访问对应接口，没有权限就被拒绝访问，启用注解@EnableGlobalMethodSecurity(prePostEnabled=true)。

--- springboot-security/src/main/java/com/kyle/springboot/security/config/WebSecurityConfig.java
+++ springboot-security/src/main/java/com/kyle/springboot/security/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.kyle.springboot.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,7 +39,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         .password("password")
                         .roles("USER")
                         .build();
+        UserDetails admin =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("password")
+                        .roles("ADMIN")
+                        .build();
 
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
--- springboot-security/src/main/java/com/kyle/springboot/security/controller/SecurityController.java
+++ springboot-security/src/main/java/com/kyle/springboot/security/controller/SecurityController.java
@@ -1,9 +1,24 @@
 package com.kyle.springboot.security.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/security")
 public class SecurityController {
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER')")
+    public ResponseEntity role() {
+        return ResponseEntity.ok("Role user is ok");
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity roleA() {
+        return ResponseEntity.ok("Role admin is ok");
+    }
 }
