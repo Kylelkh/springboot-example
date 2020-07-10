46dfc315ebfd430c955f23ca8224cda74e55d598
Kyle Liu <5201314.lkh@gmail.com>
2020-07-10 23:25:49 +0800

    add security config.

---
+++ springboot-security/src/main/java/com/kyle/springboot/security/controller/SecurityController.java
@@ -0,0 +1,9 @@
+package com.kyle.springboot.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/security")
+public class SecurityController {
+}
