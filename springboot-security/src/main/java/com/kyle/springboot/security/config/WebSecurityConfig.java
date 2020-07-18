20ca8e5e0d6dc914499c0f43833f39f75d4c6f6b
Kyle Liu <5201314.lkh@gmail.com>
2020-07-18 21:57:58 +0800

    remove extend annotation.

--- springboot-security/src/main/java/com/kyle/springboot/security/config/WebSecurityConfig.java
+++ springboot-security/src/main/java/com/kyle/springboot/security/config/WebSecurityConfig.java
@@ -1,14 +1,12 @@
 package com.kyle.springboot.security.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
